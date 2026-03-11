870b41bb38cbb9f61b6206378c7fa101698dc54c
rene-sh <rene.salinashelguero@postgrado.univalle.edu>
2026-03-11 01:21:10 +0000

    commit 2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
-  apiUrl: 'https://maneja-tus-gastos-api-bqqs.onrender.com'
+  apiUrl: 'https://manej-tus-gastos-api-bqqs.onrender.com'
 };
+
 
